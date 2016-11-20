d0d66eff7af6468e13838b4f2d3dc3c877c40ae2
Verna Koskinen <darth.kipsu@gmail.com>
2016-11-20 19:33:22 +0200

    list ingredients in a table

--- script/services/ingredient.js
+++ script/services/ingredient.js
@@ -1,17 +1,18 @@
 app.service('ingredientService', ['$http', function($http) {
     function parseToObject(data) {
         var lines = data.split("\n");
-        var ingredients = {};
+        var ingredients = [];
         for (let ingredient of lines) {
             var split = ingredient.split(",");
             if (split[0]) {
-                ingredients[split[0]] = {
+                ingredients.push({
+                    name: split[0],
                     measure: split[1],
                     calories: parseFloat(split[2]),
                     carbs: parseFloat(split[3]),
                     fat: parseFloat(split[4]),
                     proteins: parseFloat(split[5])
-                }
+                })
             }
         }
         return ingredients;
