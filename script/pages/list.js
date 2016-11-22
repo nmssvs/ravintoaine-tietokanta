51f16f73285bfd49486c6e5445ca421a8460e848
Verna Koskinen <darth.kipsu@gmail.com>
2016-11-22 16:43:19 +0200

    allow ordering ingredient list by properites

--- script/pages/list.js
+++ script/pages/list.js
@@ -1,7 +1,17 @@
 app.controller('listController', ['$scope', '$log', 'ingredientService', 
     function($scope, $log, ingredientService) {
+        $scope.ordering = 'name';
+
         ingredientService.getData().then(function(data) {
             $scope.data = data;
         });
+
+        $scope.orderBy = function(order) {
+            if ($scope.ordering == order) {
+                $scope.ordering = '-' + order;
+            } else {
+                $scope.ordering = order;
+            }
+        }
     }
 ]);
