410dbe4ef1f4c8e8a428dffa905507e249060eef
Verna Koskinen <darth.kipsu@gmail.com>
2016-11-22 21:41:55 +0200

    calculate nutrients for a meal

--- script/pages/home.js
+++ script/pages/home.js
@@ -3,6 +3,12 @@ app.controller('homeController', ['$scope', 'ingredientService', 'mealService', 
         $scope.selectedIngredient = undefined;
         $scope.ordering = undefined;
         $scope.newIngredient = undefined;
+        $scope.servings = 2;
+
+        $scope.calories = 0;
+        $scope.proteins = 0;
+        $scope.carbs = 0;
+        $scope.fat = 0;
 
         function updateMealList() {
             mealService.getData().then(function(data) {
@@ -10,11 +16,36 @@ app.controller('homeController', ['$scope', 'ingredientService', 'mealService', 
             });
         }
 
+        function updateNutrients() {
+            $scope.calories = 0;
+            $scope.proteins = 0;
+            $scope.carbs = 0;
+            $scope.fat = 0;
+
+            for (var food of $scope.ingredients) {
+                var amount = food.amount;
+                if (food.measure == '100g') {
+                    amount /= 100;
+                }
+                amount /= $scope.servings;
+
+                $scope.calories += amount * food.calories;
+                $scope.proteins += amount * food.proteins;
+                $scope.carbs += amount * food.carbs;
+                $scope.fat += amount * food.fat;
+            }
+        }
+
         updateMealList();
 
         ingredientService.getData().then(function(data) {
             $scope.list = data;
         });
+
+        $scope.update = function() {
+            updateMealList();
+            updateNutrients();
+        }
 
         $scope.selectIngredient = function(data) {
             var ingredient = data;
@@ -26,16 +57,19 @@ app.controller('homeController', ['$scope', 'ingredientService', 'mealService', 
             mealService.add(ingredient);
             $scope.selectedIngredient = undefined;
             updateMealList();
+            updateNutrients();
         }
 
         $scope.updateAmount = function(ingredient) {
             mealService.update(ingredient);
             updateMealList();
+            updateNutrients();
         }
 
         $scope.remove = function(ingredient) {
             mealService.remove(ingredient);
             updateMealList();
+            updateNutrients();
         }
 
         $scope.addNewIngredient = function() {
